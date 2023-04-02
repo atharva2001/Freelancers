8156df43c8ddd8770e9d31457834ca3a8ba707c9
Atharva Shirke <shreyahsike22july@gmail.com>
2023-04-02 20:29:07 +0530

    API changes

--- Registration/views.py
+++ Registration/views.py
@@ -216,7 +216,7 @@ def ai(request):
 def storeData(request):
     if request.method == "POST":
         prompt = request.POST.get("question")
-        openai.api_key = os.environ["OPENAI_API_KEY"]
+        openai.api_key = "YOUR API KEY HERE"
         response = openai.Completion.create(
         model="text-davinci-003",
         prompt = prompt,

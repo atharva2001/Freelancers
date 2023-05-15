64d05d0888310d6740ab3ca9f4c3a4713b95ce20
Atharva Shirke <shreyahsike22july@gmail.com>
2023-05-15 11:33:00 +0530

    forums

--- Forums/urls.py
+++ Forums/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add, name='add'),
+    path('read', views.read, name='read'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # if settings.DEBUG:
--- Forums/views.py
+++ Forums/views.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from django.shortcuts import redirect, render
 from Forums.models import tags, forum, replies
 from datetime import datetime
-
+import copy
 # Home Page of Forums
 def index(request):
    content = {}
@@ -11,35 +11,47 @@ def index(request):
 
    if request.method == "POST":
 
-      title = request.POST.get("title")
-      tag = request.POST.get("tags")
-      desc = request.POST.get("description")
-      author_name = request.POST.get("author_name")
-      email = request.POST.get("email")
-      image = request.FILES.get("image")
-
-      # temp_i = str(image).replace(" ", "_")
-      # image = temp_i
-      arr_tag = tag.split(",")
-
-      temp_db_f = forum.objects.all().last()
-      if temp_db_f != None:
-         curr_f_id = temp_db_f.id + 1
+      but = request.POST.get("submit")
+      if but == "reply":
+         print(request.session['id'])
+         answer = request.POST.get("answer")
+         author_name = request.POST.get("author_name")
+         email = request.POST.get("email")
+         image = request.FILES.get("image")
+         rep = replies(forum_id=request.session['id'], author_name=author_name,
+                       answer=answer,image=image)
+         rep.save()
       else:
-         curr_f_id = 1
 
-      for i in arr_tag:
-         if tags.objects.filter(name=i.lower()).exists():
-            temp_t = tags.objects.get(name=i)
-            temp_t.content += "," + str(curr_f_id)
-            temp_t.save()
+         title = request.POST.get("title")
+         tag = request.POST.get("tags")
+         desc = request.POST.get("description")
+         author_name = request.POST.get("author_name")
+         email = request.POST.get("email")
+         image = request.FILES.get("image")
+
+         # temp_i = str(image).replace(" ", "_")
+         # image = temp_i
+         arr_tag = tag.split(",")
+
+         temp_db_f = forum.objects.all().last()
+         if temp_db_f != None:
+            curr_f_id = temp_db_f.id + 1
          else:
-            tg = tags(name=i.lower(), content=str(curr_f_id))
-            tg.save()
-      print(datetime.now())
-      temp_f = forum(email=email, title=title, tags=tag, author_name=author_name, 
-                     description=desc, date=datetime.now(), image=image)
-      temp_f.save()
+            curr_f_id = 1
+
+         for i in arr_tag:
+            if tags.objects.filter(name=i.lower()).exists():
+               temp_t = tags.objects.get(name=i)
+               temp_t.content += "," + str(curr_f_id)
+               temp_t.save()
+            else:
+               tg = tags(name=i.lower(), content=str(curr_f_id))
+               tg.save()
+         print(datetime.now())
+         temp_f = forum(email=email, title=title, tags=tag, author_name=author_name, 
+                        description=desc, date=datetime.now(), image=image)
+         temp_f.save()
 
       
 
@@ -51,6 +63,7 @@ def index(request):
    
    for i in db_forum:
       content[i.id] = {
+         'id' : i.id,
          'name' : i.author_name,
          'title' : i.title,
          'desc' : i.description,
@@ -70,3 +83,37 @@ def index(request):
 
 def add(request):
    return render(request, 'forums/editor.html')
+
+def read(request):
+   if request.method == "POST":
+      request.session['id'] = request.POST.get('id')
+      form_content = {}
+      reply_content = []
+      forums = forum.objects.filter(id=request.session['id'])
+      is_reply = replies.objects.filter(forum_id=request.session['id']).exists()
+      reply = replies.objects.filter(forum_id=request.session['id'])
+      for i in forums:
+         form_content = {
+            'email' : i.email,
+            'title' : i.title,
+            'tags' : i.tags,
+            'name' : i.author_name,
+            'desc' : i.description,
+            'date' : i.date,
+            'link' : "media/"+str(i.image)
+         }
+         if is_reply:
+            for i in reply:
+               temp = {
+                  'name' : i.author_name,
+                  'answer' : i.answer,
+                  'date' : i.date,
+                  'link' : "media/"+str(i.image)
+               }
+               # cp = copy.deepcopy(temp)
+               reply_content.append(temp)
+            
+         # print(reply_content)
+
+      
+   return render(request, 'forums/read.html', {'content' : form_content, 'reply' : reply_content})
